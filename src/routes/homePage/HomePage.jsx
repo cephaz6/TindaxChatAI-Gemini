1970d994215df705686e684f0f2ae4972388e558
Cephaz6 <cephasblog@gmail.com>
2024-08-28 21:25:30 +0100

    Homepage setUp

--- src/routes/homePage/HomePage.jsx
+++ src/routes/homePage/HomePage.jsx
@@ -4,7 +4,17 @@ import "./homePage.css";
 const Homepage = () => {
   return (
     <div className="homepage">
-      <Link to="/dashboard">Dashboard</Link>
+      <img src="./orbital.png" alt="" className="orbital" />
+      <div className="left">
+        <h1 className="heading-font">TINDAX.AI</h1>
+        <h2>Superchare your Creativity and Productivity</h2>
+        <h3>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor
+          tenetur itaque est blanditiis voluptates.
+        </h3>
+        <Link to="/dashboard">Get Started</Link>
+      </div>
+      <div className="right"></div>
     </div>
   );
 };
