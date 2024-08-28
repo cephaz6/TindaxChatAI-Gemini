5393bb89f37d651fdd45e1083953eab250791b33
Cephaz6 <cephasblog@gmail.com>
2024-08-28 20:24:21 +0100

    Authentication setUp Using Clerk

--- src/routes/homePage/HomePage.jsx
+++ src/routes/homePage/HomePage.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
 import "./homePage.css";
 
 const Homepage = () => {
-  return <div className="homepage"></div>;
+  return (
+    <div className="homepage">
+      <Link to="/dashboard">Dashboard</Link>
+    </div>
+  );
 };
 
 export default Homepage;
