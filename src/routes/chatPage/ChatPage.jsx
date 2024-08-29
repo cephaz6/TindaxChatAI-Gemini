1e2b475b51218c0281aa75d926620cc9f94b01b7
Cephaz6 <cephasblog@gmail.com>
2024-08-29 18:47:21 +0100

    Single Chat Page SetUp

--- src/routes/chatPage/ChatPage.jsx
+++ src/routes/chatPage/ChatPage.jsx
@@ -1,7 +1,26 @@
 import "./chatPage.css";
 
 const ChatPage = () => {
-  return <div className="chatPage">ChatPage</div>;
+  return (
+    <div className="chatPage">
+      <div className="wrapper">
+        <div className="chat">
+          <div className="message user">Message from User</div>
+          <div className="message">Demo Message from AI</div>
+          <div className="message user">Message from User</div>
+          <div className="message">Demo Message from AI</div>
+          <div className="message user">Message from User</div>
+          <div className="message">Demo Message from AI</div>
+          <div className="message user">Message from User</div>
+          <div className="message">Demo Message from AI</div>
+          <div className="message user">Message from User</div>
+          <div className="message">Demo Message from AI</div>
+          <div className="message user">Message from User</div>
+          <div className="message">Demo Message from AI</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChatPage;
