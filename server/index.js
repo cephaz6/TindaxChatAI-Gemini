32eec23a53a00af53020f4db84bb78558cb9faf7
Cephaz6 <cephasblog@gmail.com>
2024-08-31 18:20:22 +0100

    Image Upload Server - ImageKIT

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import ImageKit from "imagekit";
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+const imagekit = new ImageKit({
+  urlEndpoint: process.env.IMAGE_KIT_ENDPOINT,
+  publicKey: process.env.IMAGE_KIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGE_KIT_PRIVATE_KEY,
+});
+
+app.get("/api/upload", (req, res) => {
+  const result = imagekit.getAuthenticationParameters();
+  res.send(result);
+});
+
+app.listen(port, () => {
+  console.log("Server running on 3000");
+});
