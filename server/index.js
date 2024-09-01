9a60e1b706601bfd1e98168fd824f09ae321b26e
Cephaz6 <cephasblog@gmail.com>
2024-09-01 13:09:47 +0100

    Setting up IMAGE_KIT and CORS

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 import express from "express";
 import ImageKit from "imagekit";
+import cors from "cors";
 
 const port = process.env.PORT || 3000;
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  })
+);
 
 const imagekit = new ImageKit({
   urlEndpoint: process.env.IMAGE_KIT_ENDPOINT,
