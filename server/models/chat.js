e45aae6267e8152447a35e7b87556d9b45294fe1
Cephaz6 <cephasblog@gmail.com>
2024-09-04 02:09:13 +0100

    Fix

--- server/models/chat.js
+++ server/models/chat.js
@@ -10,7 +10,7 @@ const chatSchema = new mongoose.Schema(
       {
         role: {
           type: String,
-          enum: ["user" | "model"],
+          enum: ["user", "model"],
           required: true,
         },
         parts: [
