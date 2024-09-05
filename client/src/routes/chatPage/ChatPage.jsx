9b6de01af7e45d3439a756f502459d396b8e4826
Cephaz6 <cephasblog@gmail.com>
2024-09-05 02:56:05 +0100

    Server Updated for Conversation Cummlation

--- client/src/routes/chatPage/ChatPage.jsx
+++ client/src/routes/chatPage/ChatPage.jsx
@@ -1,28 +1,54 @@
 import "./chatPage.css";
 import NewPrompt from "../../components/newPrompt/NewPrompt";
+import { useQuery } from "@tanstack/react-query";
+import { useLocation } from "react-router-dom";
+import Markdown from "react-markdown";
+import { IKImage } from "imagekitio-react";
 
 const ChatPage = () => {
+  const path = useLocation().pathname;
+  const chatId = path.split("/").pop();
+
+  const { isPending, error, data } = useQuery({
+    queryKey: ["chat", chatId],
+    queryFn: () =>
+      fetch(`${import.meta.env.VITE_SERVER_URL}/api/chats/${chatId}`, {
+        credentials: "include",
+      }).then((res) => res.json()),
+  });
+
+  console.log(data);
   return (
     <div className="chatPage">
       <div className="wrapper">
         <div className="chat">
-          <div className="message user">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora
-            dolor, laboriosam amet, nisi neque sed perferendis ipsa quae qui
-            impedit ratione cupiditate sunt tenetur corrupti eos nobis
-            molestiae.
-          </div>
-          <div className="message">Demo Message from AI</div>
-          <div className="message user">Message from User</div>
-          <div className="message">Demo Message from AI</div>
-          <div className="message user">Message from User</div>
-          <div className="message">Demo Message from AI</div>
-          <div className="message user">Message from User</div>
-          <div className="message">Demo Message from AI</div>
-          <div className="message user">Message from User</div>
-          <div className="message">Demo Message from AI</div>
-          <div className="message user">Message from User</div>
-          <div className="message">Demo Message from AI</div>
+          {isPending
+            ? "Loading..."
+            : error
+            ? "Something went wrong"
+            : data?.history.map((message, i) => (
+                <>
+                  {message.img && (
+                    <IKImage
+                      urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
+                      path={message.img}
+                      height="300"
+                      width="400"
+                      transformation={[{ height: 300, width: 400 }]}
+                      loading="lazy"
+                      lqip={{ acive: true, quality: 20 }}
+                    />
+                  )}
+                  <div
+                    className={
+                      message.role === "user" ? "message user" : "message"
+                    }
+                    key={i}
+                  >
+                    <Markdown>{message.parts[0].text}</Markdown>
+                  </div>
+                </>
+              ))}
 
           <NewPrompt />
         </div>
--- client/src/routes/dashboardPage/DashboardPage.jsx
+++ client/src/routes/dashboardPage/DashboardPage.jsx
@@ -1,23 +1,38 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import "./dashboardPage.css";
-import { useAuth } from "@clerk/clerk-react";
+import { useNavigate } from "react-router-dom";
 
 const DashboardPage = () => {
-  const { userId } = useAuth();
+  // Access the client
+  const queryClient = useQueryClient();
+
+  const navigate = useNavigate();
+
+  // Mutations
+  const mutation = useMutation({
+    mutationFn: (text) => {
+      return fetch(`${import.meta.env.VITE_SERVER_URL}/api/chats`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text }),
+      }).then((res) => res.json());
+    },
+    onSuccess: (id) => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries({ queryKey: ["userChats"] });
+      navigate(`/dashboard/chats/${id}`);
+    },
+  });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const text = e.target.text.value;
 
     if (!text) return;
-
-    await fetch("http://localhost:3000/api/chats", {
-      method: "POST",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ text }),
-    });
+    mutation.mutate(text);
   };
   return (
     <div className="dashboardPage">
